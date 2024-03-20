6ce47a9031e23662544d3f078663922f3e963af2
Roberto Reta <robbie.reta67@gmail.com>
2024-03-20 21:07:54 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Sidebar from "./ui/sidebar";
+
+export default function Home() {
+  return;
+
+  <main className={styles.main}>
+    a
+    <Sidebar />
+  </main>;
+}
